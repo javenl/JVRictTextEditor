961161e2a54a29c0138a07b4cff7ebea001990c3
Javen <412775083@qq.com>
2016-11-25 10:58:29 +0800

    Initial Commit

---
+++ JVRichTextEditor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JVRichTextEditor
+//
+//  Created by liu on 16/11/25.
+//  Copyright © 2016年 liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JVRichTextEditor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JVRichTextEditor
+//
+//  Created by liu on 16/11/25.
+//  Copyright © 2016年 liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
